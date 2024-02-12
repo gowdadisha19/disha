35e971f47bd51efaca4cecb6624fc9331c3ccf68
dishaa <dishaa19@gmail.com>
2024-02-12 15:47:40 +0530

    change3

--- hello.java
+++ hello.java
@@ -2,7 +2,9 @@ public class hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("hello tumkur");
+		System.out.println("hello tumkur ");
+		System.out.println("benglore");
+
 	}
 }
 
